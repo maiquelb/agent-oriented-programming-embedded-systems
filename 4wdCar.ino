ce496d74c11cc4937e821d9ce168dcc20d0f3077
nischeur <nischeur@gmail.com>
2019-10-11 10:41:48 -0300

    Create 4wdCar.ino
    Basic Arduino Implementation on a 4wd mini robot

---
+++ 4wdCar.ino
@@ -0,0 +1,95 @@
+#include <AFMotor.h>
+
+  AF_DCMotor  motorFR(1);
+  AF_DCMotor  motorBR(4);
+  AF_DCMotor  motorFL(2);
+  AF_DCMotor  motorBL(3);
+  
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+  // VELOCIDADE DOS MOTORES
+  motorFR.setSpeed(255);
+  motorBR.setSpeed(255);
+  motorFL.setSpeed(255);
+  motorBL.setSpeed(255);
+  
+
+  // INICIAM PARADOS
+  motorFR.run(RELEASE);
+  motorBR.run(RELEASE);
+  motorFL.run(RELEASE);
+  motorBL.run(RELEASE);
+
+  
+}
+
+void loop() {
+
+  delay(3000);
+  
+  moveForward();
+  delay(1000);
+  halt();
+  delay(500);
+
+  moveRight();
+  delay(1600);
+  halt();
+  delay(500);
+
+  moveForward();
+  delay(800);
+  halt();
+  delay(500);
+  
+  //moveRight();
+  motorFL.run(BACKWARD);
+  motorBL.run(BACKWARD);
+  delay(3500);
+  halt();
+  delay(500);
+  
+  moveForward();
+  delay(1000);
+  halt();
+  delay(500);
+   
+}
+  void moveForward() {
+    motorFR.run(FORWARD);
+    motorBR.run(FORWARD);
+    motorFL.run(FORWARD);
+    motorBL.run(FORWARD);
+    
+  }
+
+  void moveBackward() {
+    motorFR.run(BACKWARD);
+    motorBR.run(BACKWARD);
+    motorFL.run(BACKWARD);
+    motorBL.run(BACKWARD);
+  }
+
+  void moveRight() {
+    motorFR.run(RELEASE);
+    motorBR.run(RELEASE);
+    motorFL.run(FORWARD);
+    motorBL.run(FORWARD);
+  }
+
+  void moveLeft() {
+    motorFR.run(FORWARD);
+    motorBR.run(FORWARD);
+    motorFL.run(RELEASE);
+    motorBL.run(RELEASE);
+  }
+
+  void halt() {
+    motorFR.run(RELEASE);
+    motorFL.run(RELEASE);
+    motorBR.run(RELEASE);
+    motorBL.run(RELEASE);
+  }
